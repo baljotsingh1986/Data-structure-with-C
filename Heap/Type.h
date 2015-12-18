1c57e93261b7210594394c898fc4bec617333154
Baljot Singh <baljotsingh1986@yahoo.com>
2015-12-18 12:47:04 -0800

    Create Type.h

---
+++ Heap/Type.h
@@ -0,0 +1,18 @@
+/*File name: Type.h
+ * Author: Baljot Singh
+ * Description:Header file to define the type of
+ *             data type which will be used in Heap
+ *             */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#ifndef __TYPE_H
+#define __TYPE_H
+
+#ifndef TYPE
+#define TYPE   int
+#endif
+
+
+#endif
