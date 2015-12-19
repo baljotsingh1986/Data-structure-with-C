d5d1280f9be5d09dbf5493ba117908d23361a051
Baljot Singh <baljotsingh1986@yahoo.com>
2015-12-19 10:43:10 -0800

    Update LinkedList.c

--- HashMap/LinkedList.c
+++ HashMap/LinkedList.c
@@ -30,7 +30,7 @@ struct Iterator
 /*Helper function ro Build a new Node*/
 struct node* _newNode()
 {
-	struct node* newNode  = malloc(sizeof(struct node));
+	struct node* newNode  = (struct node*)malloc(sizeof(struct node));
 	newNode->next = NULL;
 	return newNode;
 }
@@ -39,7 +39,7 @@ struct node* _newNode()
 struct LinkedList* initLinkedList()
 {
 	struct LinkedList* list = NULL;
-	list = malloc(sizeof(struct LinkedList));
+	list = (struct LinkedList*)malloc(sizeof(struct LinkedList));
 	list->head = _newNode();
 	list->count = 0;
 	return list;
@@ -48,7 +48,7 @@ struct LinkedList* initLinkedList()
 /*Function to initialize Iterator*/
 struct Iterator* initIterator(struct LinkedList* list)
 {
-	struct Iterator* iter = malloc(sizeof(struct Iterator));
+	struct Iterator* iter = (struct Iterator*)malloc(sizeof(struct Iterator));
 	iter->cur = list->head;
 	return iter; 
 }
