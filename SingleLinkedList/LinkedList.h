0f2599f7c7615751b899628c8a083c9e7ac61b89
Baljot Singh <baljotsingh1986@yahoo.com>
2015-12-19 18:58:36 -0800

    Update LinkedList.h

--- SingleLinkedList/LinkedList.h
+++ SingleLinkedList/LinkedList.h
@@ -11,19 +11,33 @@
 #define LINKEDLIST_H
 
 struct LinkedList;
+
 struct Iterator;
+
 struct LinkedList* initLinkedList();
+
 struct Iterator* initIterator(struct LinkedList* list);
+
 void addFront(const char* val, struct LinkedList* list);
+
 void printList(struct LinkedList* list);
+
 void addLast(const char* val, struct LinkedList* list);
+
 int addBefore(const char* newVal, const char* existingVal, struct LinkedList* list);
+
 int addAfter(const char* newVal, const char* existingVal, struct LinkedList* list);
+
 int removeNode(const char* val, struct LinkedList* list);
+
 int getLength(struct LinkedList* list);
 
+void reverseList(struct LinkedList* list);
+
 int hasNext(struct Iterator* iter);
+
 char* next(struct Iterator* iter);
+
 void freeList(struct LinkedList* list);
 
 #endif
